68030260a0b8d13d2e68c427833185f9a322ebaa
Mark Raymond <raymond.ayade@gmail.com>
2021-09-22 02:11:40 +0800

    added PostModel and CommentModel

--- src/redux/model.ts
+++ src/redux/model.ts
@@ -96,6 +96,18 @@ export type ClassModel = {
   };
 };
 
+export type PostModel = {
+  author: string,
+  created: string,
+  content: string,
+  comment: CommentModel[]
+};
+
+export type CommentModel = {
+  author: string,
+  content: string
+};
+
 interface ClassesCallback {
   (classes: ClassesModel): void;
 }
