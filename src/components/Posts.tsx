acfb667e642045f140b604b3cb7d1ee36363a716
Mark Raymond <raymond.ayade@gmail.com>
2021-09-22 02:14:34 +0800

    changing into super Post component that holds createPost and Post component

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,36 +1,83 @@
-import { IonCard, IonIcon, IonAvatar, IonButton, IonText, IonItemDivider, IonInput, IonTextarea } from "@ionic/react";
+import { IonCard, IonIcon, IonAvatar, IonButton, IonText, IonTextarea, useIonModal } from "@ionic/react";
 import { personCircleOutline, ellipsisVertical } from "ionicons/icons";
 import React, { useState } from "react";
 import { Box } from "@material-ui/core";
 import MUIRichTextEditor from "mui-rte";
 
+import { PostModel, CommentModel } from "../redux/model";
+
 import "./Posts.css"
 
 
-const CreatePost: React.FC = () => {
-    const [postBox, setPostBox] = useState(false);
 
-    const togglePostBox = () => {
-        setPostBox(!postBox);
+
+const tempData = [
+    {
+        author : "Teacher Tanquis",
+        created : "Yesterday",
+        content: "Ohayo Gozaimasu",
+        comments: [
+            {
+                author: "Student Kimberly",
+                content: "Hello"
+            },
+            {
+                author: "Student Mark Joseph",
+                content: "Hello Miss"
+            }
+        ]
+    },
+    {
+        author : "Teacher Tanquis",
+        created : "Today",
+        content: "Minasan",
+        comments: [
+            {
+                author: "Student Raymond",
+                content: "Hello World"
+            },
+            {
+                author: "Student Kimberly",
+                content: "Hello"
+            }
+        ]
     }
+];
+
+const Posts: React.FC = () => {
+    return (
+        <>           
+            <CreatePost />
+            { tempData.map((post, index) => (
+                <Post author={post.author} created={post.created} content={post.content} comment={post.comments} />
+            ))}
+            
+        </>
+    );
+};
+
+const CreatePost: React.FC = () => {
+    const handleDismiss = () => {
+        dismiss();
+    };
+
+    const [present, dismiss] = useIonModal(Editor, { onDismiss: handleDismiss });
 
     return (
         <IonCard className="post-box">
-            { !postBox ? (
-             <div className="avatar-txtbox-section" onClick={togglePostBox}>
+             <div className="avatar-txtbox-section">
                 <IonAvatar className="avatar" style={{ height: 45, width: 45 }}>
                     <IonIcon
                     src={personCircleOutline}
                     style={{ width: "100%", height: "100%", background: "#fff" }}
                     />
                 </IonAvatar>
-                <div className="start-discussion-box">
+                <div className="start-discussion-box" onClick={()=>{
+                    present({ cssClass: 'editorModal' });
+                }}>
                     Start a discussion
                 </div>               
-            </div>
-            ) : (
-                <MUIRichTextEditor label="Start a discussion" />
-            )}
+            </div>                
             <div className="post-btn-section">
                 <IonButton className="post-btn" color="secondary">
                     Post
@@ -40,7 +87,20 @@ const CreatePost: React.FC = () => {
     );
 };
 
-const Post: React.FC = () => {
+const Editor: React.FC<{onDismiss: () => void}> = ({ onDismiss }) => {
+
+    return (
+        <div className="modal">
+            <MUIRichTextEditor label="Start a discussion" />
+            <div className="editor-btn-row">
+                <IonButton onClick={ onDismiss }>Cancel</IonButton>
+                <IonButton>Post</IonButton>
+            </div>
+        </div>
+    );
+};
+
+const Post: React.FC<PostModel> = (props) => {
     return (
         <IonCard className="post-box">  
             <div className="post-header-section">
@@ -51,8 +111,8 @@ const Post: React.FC = () => {
                     />
                 </IonAvatar>
                 <div className="name-time-section">
-                    <IonText className="author">Author Name</IonText>
-                    <IonText>1 minutes ago</IonText>
+                    <IonText className="author">{props.author}</IonText>
+                    <IonText>{props.created}</IonText>
                 </div>
                 <IonAvatar className="ellipsis" style={{ height: 45, width: 45 }}>
                     <IonIcon
@@ -62,14 +122,15 @@ const Post: React.FC = () => {
                 </IonAvatar>
             </div>
             <div className="post-content-section">
-                <IonText className="content">Discussion Content</IonText>
+                <IonText className="content">{props.content}</IonText>
             </div>
             <div className="post-comment-counter-section">
-                <IonText>2 comments</IonText>
+                <IonText>{props.comment.length} comments</IonText>
             </div>
             <div className="post-comment-section">
-                <Comment />
-                <Comment />
+                { props.comment.map((comment, index) => (
+                    <Comment author={comment.author} content={comment.content}/>
+                ))}
             </div>
             <div className="post-add-comment-section">
                 <IonAvatar className="avatar" style={{ height: 45, width: 45 }}>
@@ -86,7 +147,7 @@ const Post: React.FC = () => {
     );
 };
 
-const Comment: React.FC = () => {
+const Comment: React.FC<CommentModel> = (props) => {
     return (
         <div className="comment">
             <IonAvatar className="avatar" style={{ height: 45, width: 45 }}>
@@ -96,12 +157,12 @@ const Comment: React.FC = () => {
                 />
             </IonAvatar>
             <div className="comment-content">
-                <IonText className="comment-author">Comment Author</IonText>
-                <IonText>Comment Content</IonText>
+                <IonText className="comment-author">{props.author}</IonText>
+                <IonText>{props.content}</IonText>
             </div>
         </div>
     );
 };
 
 
-export { CreatePost, Post };
+export default Posts;
